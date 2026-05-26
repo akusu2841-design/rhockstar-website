f6e0d7ccc5759b50092601fb72b6b2d271562eff
akusu2841-design <akusu2841@gmail.com>
2026-05-26 13:15:03 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ apiKey: "AIzaSyCKdJaHy_FUSFbnUkTCm3p7p32sM1E5r7w",
   messagingSenderId: "707658422879",
   appId: "1:707658422879:web:467e0785561454aca999ea"
 };
-
+alert("firebase connected");
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
